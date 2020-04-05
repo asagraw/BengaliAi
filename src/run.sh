0e9d6263b4222d37de77d11e108c4b7b15c7bd52
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-04-05 08:03:27 +0000

    first commit

---
+++ src/run.sh
@@ -0,0 +1,12 @@
+export CUDA_VISIBLE_DEVICES=0
+export IMG_HEIGHT=137
+export IMG_WIDTH=236
+export EPCOHS=50
+export TRAIN_BATCH_SIZE=64
+export TEST_BATCH_SIZE=8
+export MODEL_MEAN="(0.485,0.456,0.406)"
+export MODEL_STD="(0.229, 0.224, 0.225)"
+export BASE_MODEL="resnet34"
+
+export TRAINING_FOLDS="(0,1,2,3)"
+export VALIDATION_FOLDS="(4,)"
